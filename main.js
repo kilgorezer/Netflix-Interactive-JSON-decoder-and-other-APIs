043d16448844dc1f97b13274223b37c3e199f7a9
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2024-11-30 19:44:53 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,6 @@
 var netint = Object.create(null); // An object to store other modules
 (async function() {
   netint.javakilg = Function((await (await fetch("https://javakilg.kilgorezer.com/releases/A2.js")).text()) + "return JavaKilg")();
+  // more imports here
+  netint.ready = true;
 })();

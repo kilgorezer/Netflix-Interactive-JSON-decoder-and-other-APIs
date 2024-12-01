18f8520a03b32093d23a676335972ecaceaca10f
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2024-11-30 19:34:03 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-// Main API Code
+// Utility Setup Code
+var netint = Object.create(null); // An object to store other modules
+(async function() {
+  netint.javakilg = Function((await (await fetch("https://javakilg.kilgorezer.com/releases/A2.js")).text()) + "return JavaKilg");;
+})();

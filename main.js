a2327fddcf77b6451dde03cd9f337f08d5282a76
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2024-11-30 19:37:43 -0800

    Fix main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Utility Setup Code
 var netint = Object.create(null); // An object to store other modules
 (async function() {
-  netint.javakilg = Function((await (await fetch("https://javakilg.kilgorezer.com/releases/A2.js")).text()) + "return JavaKilg");;
+  netint.javakilg = Function((await (await fetch("https://javakilg.kilgorezer.com/releases/A2.js")).text()) + "return JavaKilg")();
 })();
